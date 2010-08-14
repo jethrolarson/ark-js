3ac864f915474fcce5e514a43d063881a6377201
Daniel Buchner <danieljb2@gmail.com>
2010-08-14 02:16:42 -0700

    fuck

---
+++ main.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import cgi
+import datetime
+import os
+import re
+import sys
+import urllib
+import urlparse
+
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import util
+from google.appengine.ext.webapp import template
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+# Set to true if we want to have our webapp print stack traces, etc
+_DEBUG = True
+
+
+class MainHandler(webapp.RequestHandler):
+
+  def get(self):
+    directory = os.path.dirname(__file__)
+    path = os.path.join(directory, os.path.join('templates', "index.html"))
+    self.response.out.write(template.render(path, "", debug=_DEBUG))
+
+
+def main():
+  application = webapp.WSGIApplication([('/', MainHandler)],
+                                       debug=True)
+  util.run_wsgi_app(application)
+
+
+if __name__ == '__main__':
+  main()
