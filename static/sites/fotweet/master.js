5ef12cf5210a50410cadba3348d6b923d4aca052
Jethro <jethrolarson@gmail.com>
2010-08-15 14:00:58 -0700

    removed fauxbook. Working on master client

--- static/sites/fotweet/master.js
+++ static/sites/fotweet/master.js
@@ -1,28 +1,27 @@
 $(function(){
-  tweetText = $('#tweetText');
-  tweets = $('#tweets');
-  feed = $('#feed');
+	tweetText = $('#tweetText');
+	tweets = $('#tweets');
+	feed = $('#feed');
 
-  names = ["bob123", "monktacular", "bobbytables", "ManWithHat"];
-  messages = ["I love app engine", "Going to the hockey game.", "I'm drunk", "Oh man me too", "Has anyone seen Inception?", "This isn't my status"];
+	names = ["bob123", "monktacular", "bobbytables", "ManWithHat"];
+	messages = ["We should totally add more text", "I agree. It would make it less stale", "Yeah, I'm said that twice already","I love app engine", "Going to the hockey game.", "I'm drunk", "Oh man me too", "Has anyone seen Inception?", "This isn't my status"];
 
-  $('#newTweet').submit(function(){
-    appendTweet('@Me '+tweetText.val());
-    tweetText.val('');
-    return false;
-  });
+	$('#newTweet').submit(function(){
+		appendTweet('@Me '+tweetText.val());
+		tweetText.val('');
+		return false;
+	});
 
-  function appendTweet(text) {
-    tweets.html('<div class="update">'+text+'</div>'+tweets.html());
-		console.log("newTweet")
+	function appendTweet(text) {
+		tweets.html('<div class="update">'+text+'</div>'+tweets.html());
 		$('#newTweet').trigger("newTweet");
-  }
+	}
 
-  function createTweet() {
-    appendTweet('@' + names[Math.floor(Math.random()*names.length)] + ' ' + messages[Math.floor(Math.random() * messages.length)]);
-    setTimeout(createTweet, Math.floor(Math.random()*7000+3000));
-  }
-  setTimeout(createTweet, 500);
+	function createTweet() {
+		appendTweet('@' + names[Math.floor(Math.random() * names.length)] + ' ' + messages[Math.floor(Math.random() * messages.length)]);
+		setTimeout(createTweet, Math.floor(Math.random() * 7000 + 3000));
+	}
+	setTimeout(createTweet, 500);
 
 
 	new ArcServer({
